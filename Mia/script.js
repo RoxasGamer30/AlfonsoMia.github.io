fe878796213bab6db5f7129482a58a8440914789
RoxasGamer30 <alfonsogarcia_30@outlook.com>
2025-02-10 09:04:43 -0700

    update

---
+++ Mia/script.js
@@ -0,0 +1,16 @@
+
+   /*TEXTO WELCOME*/
+   var i = 0;
+   var txt = "Welcome to My Personal Website";
+   var speed = 80;
+
+   function TypeWrite()
+   {
+       if(i < txt.length)
+       {
+        document.getElementById("Texto").innerHTML += txt.charAt(i);
+        i++;
+           setTimeout(TypeWrite,speed);
+       }
+   }   
+   TypeWrite();
